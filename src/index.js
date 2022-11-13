08d4234fb3461aa0f469b2ad0094599c3e62d72d
diego <guatemaladiegojose@gmail.com>
2022-11-12 20:44:35 -0500

    commit tree 0c9560523ebe351c6177b745d3b6947e56707419

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 const init = () => {
   
+    const inputForm= document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event)=> {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+
+        //console.log(input.value);
+        //console.log(event);  //1// this way works as well
+        //event.target.children[1].value //1// this way works as well
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data=> {
+            //console.log(data); //2//
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+            
+
+        })
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
